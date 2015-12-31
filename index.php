68be78160066eef411c1fd7d827269aced00c453
srmishra <samatwork14@gmail.com>
2015-12-31 17:11:59 +0530

    index page added

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8" />
+    <title>cmd player</title>
+    <meta name="author" content="sam mishra"/>
+    <meta name="keywords" content="command line os,cmd os,cmd.to,cmd.fm">
+    <meta name="description" content="command-line interface for web">
+    <link rel="sitemap" type="application/xml" title="Sitemap" href=""/>
+    <link rel="shortcut icon" href="favicon.ico"/>
+    <script src="js/jquery-1.7.1.min.js"></script>
+    <script src="js/jquery.mousewheel-min.js"></script>
+    <script src="js/jquery.terminal-min.js"></script>
+    <link href="css/jquery.terminal.css" rel="stylesheet"/>
+
+    <link rel="icon" type="image/png" href="favicon.png"/>
+
+  </head>
+<body>
+<script src="js/app.min.js"></script>
+</body>
+</html>
+
